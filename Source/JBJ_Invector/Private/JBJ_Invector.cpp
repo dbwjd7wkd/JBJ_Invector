6f1fb1d3995b8b3ad55e9cd66a60df4f163ba49a
injaepak <85613541+injaepak@users.noreply.github.com>
2021-07-22 18:46:30 +0900

    맵 초기 세팅

---
+++ Source/JBJ_Invector/Private/JBJ_Invector.cpp
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JBJ_Invector.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JBJ_Invector, "JBJ_Invector" );
+
+DEFINE_LOG_CATEGORY(JBJ);
+
+#define APPINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define CALLINFO() UE_LOG(JBJ, Warning, TEXT("%s"), *APPINFO)
+#define PRINTLOG(fmt, ...) UE_LOG(JBJ, Warning, TEXT("%s %s"), *APPINFO, *FString::Printf(fmt, ##__VA_ARGS__) )
---
+++ Source/JBJ_Invector/Public/JBJ_InvectorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JBJ_InvectorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JBJ_INVECTOR_API AJBJ_InvectorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
