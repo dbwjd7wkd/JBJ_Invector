5f859ddbd94b219b3e229bbafc626f93fa5553f2
injaepak <85613541+injaepak@users.noreply.github.com>
2021-07-23 03:10:28 +0900

    새 프로젝트 세팅

--- Source/JBJ_Invector/JBJ_Invector.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "JBJ_Invector.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JBJ_Invector, "JBJ_Invector" );
-
-DEFINE_LOG_CATEGORY(LogJBJ_Invector)
- 
---
+++ Source/JBJ_Invector/Public/JBJ_InvectorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JBJ_InvectorGameMode.generated.h"
+
+UCLASS(MinimalAPI)
+class AJBJ_InvectorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AJBJ_InvectorGameMode();
+};
+
+
+
